1d316f7506f8d88f201449f4332eb390c3deef9a
Walter Hardy <walterhardy@gmail.com>
2015-04-26 03:55:57 -0400

    added files related to BLE iBeacons
    not able to scan the area for iBeacons and using REST API transmit this
    to ThingWorx

---
+++ BLE_test/src/BLE_test.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : BLE_test.cpp
+ Author      : 
+ Version     :
+ Copyright   : MEDIsense
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}

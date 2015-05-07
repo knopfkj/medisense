b1a9e4f7cff68205180e2eea4b7b274497e2ab24
Walter Hardy <walterhardy@gmail.com>
2015-05-06 23:24:30 -0400

    updated iBeacon Node.js code to work with ThingWorx
    using information from Adrian at PTC I changed the RESTapi all and added
    a DataThing to ThingWorx. When executed this file (test.js) now sends
    information to ThingWorx as a JSON data structure and the thing then
    parses this data into individual values.

--- node-bleacon/test.js
+++ node-bleacon/test.js
@@ -1,5 +1,6 @@
 var Bleacon = require('./index'),
-    http = require('http');
+    http = require('http')
+var array = [];
 
 Bleacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
 
@@ -9,23 +10,24 @@ Bleacon.on('discover', function(bleacon) {
   var options = {
     host: 'thingworx-wvu.ptcmscloud.com',
     port: 80,
-    path: '/Thingworx/Things/medisenseBLEscan/Properties/BLEdata?Accept=application/json&method=put&x-thingworx-session=true&value=' + JSON.stringify(bleacon) + '&appKey=314c5fc5-a06b-4fbb-b40a-4d87e24e9bff'
-  };
+    method: 'POST',
+    path: '/Thingworx/Things/medisenseBLEscan/Properties/BLEdata?Accept=application/json&method=post&x-thingworx-session=true&value={"dataShape":{"fieldDefinitions":{"uuid":{"baseType":"STRING","description":"uuid","name":"uuid","aspects":{},"ordinal":0},"major":{"baseType":"NUMBER","description":"major","name":"major","aspects":{},"ordinal":0},"minor":{"baseType":"NUMBER","description":"minor","name":"minor","aspects":{},"ordinal":0},"measuredPower":{"baseType":"NUMBER","description":"measuredPower","name":"measuredPower","aspects":{},"ordinal":0},"rssi":{"baseType":"NUMBER","description":"rssi","name":"rssi","aspects":{},"ordinal":0},"accuracy":{"baseType":"NUMBER","description":"accuracy","name":"accuracy","aspects":{},"ordinal":0},"proximity":{"baseType":"STRING","description":"uuid","name":"uuid","aspects":{},"ordinal":0}}},"rows":[' + JSON.stringify(bleacon) + ']}&appKey=314c5fc5-a06b-4fbb-b40a-4d87e24e9bff'
+};
 
   var req = http.request(options, function(resp){
     //console.log('STATUS: ' + resp.statusCode);
     //console.log('HEADERS: ' + JSON.stringify(resp.headers));
     resp.setEncoding('utf8');
     console.log("sent data");
-    req.end();
+    //req.end();
   });
   req.on("error", function(e){
     console.log("Got error: " + e.message);
     //console.log(bleacon);
   });
   req.end();
+
+
 });
-
-
 
 Bleacon.startScanning(/*'e2c56db5dffb48d2b060d0f5a71096e0', 0, 0*/);
